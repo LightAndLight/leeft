ce7db70293e6c5b6afa5eff533d199d5e42b3831
Isaac Elliott <isaace71295@gmail.com>
2018-10-31 09:36:50 +1000

    Use cabal2.4 source-repository

--- default.nix
+++ default.nix
@@ -10,24 +10,7 @@ let
 
   variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
 
-  # subst = haskellPackages.callPackage (import ./nix/subst.nix) {};
-  # subst =
-    # haskellPackages.callPackage
-    # (pkgs.fetchFromGitHub {
-      # owner = "lightandlight";
-      # repo = "subst";
-      # rev = "10445211a173633955a1498df4a2e010e228fca8";
-      # sha256 = "0rk4k9h0l045v6ba9ar8bs9yh8s6bnchqgzifsblh20p17qiv9if";
-    # }) {};
-  # subst =
-    # pkgs.fetchFromGitHub {
-      # owner = "lightandlight";
-      # repo = "subst";
-      # rev = "10445211a173633955a1498df4a2e010e228fca8";
-      # sha256 = "0rk4k9h0l045v6ba9ar8bs9yh8s6bnchqgzifsblh20p17qiv9if";
-    # };
-  subst =
-    import (fetchTarball "https://github.com/LightAndLight/subst/archive/0.0.1.tar.gz") { inherit nixpkgs compiler; };
+  subst = haskellPackages.callPackage (import ./nix/subst.nix) {};
 
   drv = variant (haskellPackages.callPackage (import ./leeft.nix) { inherit subst; });
 
--- nix/subst.nix
+++ nix/subst.nix
@@ -4,10 +4,10 @@ mkDerivation {
   version = "0.1.0.0";
   src = fetchgit {
     url = "https://github.com/lightandlight/subst";
-    sha256 = "0rk4k9h0l045v6ba9ar8bs9yh8s6bnchqgzifsblh20p17qiv9if";
-    rev = "10445211a173633955a1498df4a2e010e228fca8";
-    fetchSubmodules = true;
+    sha256 = "08dz6gp032is2riffmwx1lrz1d0wsn2jcarfhmjk5kg8p1g4lm2c";
+    rev = "d3d42f0f3aba29c9c8e37332242e1a30caecc747";
   };
+  postUnpack = "sourceRoot+=/subst; echo source root reset to $sourceRoot";
   libraryHaskellDepends = [ base lens ];
   description = "De Bruijn indices using Backpack-y Prisms and Plated";
   license = stdenv.lib.licenses.bsd3;
